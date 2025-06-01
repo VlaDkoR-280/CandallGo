872721d3527a7dab821aad8d9131f969105508ba
VlaDkoR-280 <vladkor280@yandex.ru>
2025-06-02 01:27:41 +0300

    Rename data_last_use to date_last_use; Fixed bugs

--- internal/db/db.go
+++ internal/db/db.go
@@ -43,7 +43,7 @@ func (db *DB) GetUserData(userId string) (userData UserData, err error) {
 
 // GetGroupData получение GroupData
 func (db *DB) GetGroupData(groupId string) (groupData GroupData, err error) {
-	err = db.conn.QueryRow(context.Background(), "SELECT id, tg_id, name, isgroup, data_last_use, sub_end_date, time_last_update FROM group_data WHERE tg_id=$1", groupId).Scan(
+	err = db.conn.QueryRow(context.Background(), "SELECT id, tg_id, name, isgroup, date_last_use, sub_end_date, time_last_update FROM group_data WHERE tg_id=$1", groupId).Scan(
 		&groupData.Id, &groupData.TgId, &groupData.GroupName, &groupData.IsGroup, &groupData.DateLastUse, &groupData.SubDateEnd, &groupData.TimeLastUpdate)
 	if err != nil {
 		return GroupData{}, err
@@ -53,7 +53,7 @@ func (db *DB) GetGroupData(groupId string) (groupData GroupData, err error) {
 
 // GetGroupsOfUser получение списка групп пользователя
 func (db *DB) GetGroupsOfUser(userId string, groupList *list.List, onlyGroup bool) error {
-	rows, err := db.conn.Query(context.Background(), "SELECT g.id, g.tg_id, g.name, g.isgroup, g.data_last_use, g.sub_end_date, g.time_last_update FROM group_data g JOIN users_of_group ug ON ug.group_id=g.id JOIN user_data u ON u.id=ug.user_id WHERE u.tg_id=$1", userId)
+	rows, err := db.conn.Query(context.Background(), "SELECT g.id, g.tg_id, g.name, g.isgroup, g.date_last_use, g.sub_end_date, g.time_last_update FROM group_data g JOIN users_of_group ug ON ug.group_id=g.id JOIN user_data u ON u.id=ug.user_id WHERE u.tg_id=$1", userId)
 	if err != nil {
 		return err
 	}
