9eea733543febae28919e517849376c66336787e
VlaDkoR-280 <vladkor280@yandex.ru>
2025-06-14 18:09:11 +0300

    Added correct program stop

--- bot/main.go
+++ bot/main.go
@@ -4,22 +4,32 @@ import (
 	"CandallGo/config"
 	"CandallGo/internal/telegram"
 	"CandallGo/logs"
+	"context"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	cfg := config.LoadConfig()
 	bot, err := telegram.NewBot(cfg.BotToken)
 	if err != nil {
-		logs.SendLog(logs.LogEntry{Level: "fatal", EventType: "system", Msg: "Error creating bot", Error: err.Error()})
+		logs.SendLog(logs.LogEntry{Level: "fatal", EventType: "system", Info: "Error creating bot", Error: err.Error()})
 		return
 	}
-	defer func() {
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		<-ctx.Done()
 		bot.Close()
-		logs.SendLog(logs.LogEntry{Level: "info", EventType: "system", Msg: "Bot is stopped"})
+		logs.SendLog(logs.LogEntry{Level: "info", EventType: "system", Info: "Bot is stopped"})
+
 	}()
+
 	logs.SendLog(logs.LogEntry{
 		Level: "info", EventType: "system",
-		Msg: "Bot started",
+		Info: "Bot started",
 	})
 	bot.Start()
 
